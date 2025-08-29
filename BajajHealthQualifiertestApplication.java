e94753deda28a969c2c2ba16d73c7ff988c88deb
Pavan Kumar <pavan.22bce8932@vitapstudent.ac.in>
2025-08-29 12:21:36 +0530

    Add files via upload

---
+++ BajajHealthQualifiertestApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.bajaj_health_qualifiertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajHealthQualifiertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajHealthQualifiertestApplication.class, args);
+	}
+
+}
---
+++ BajajHealthQualifiertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.bajaj_health_qualifiertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajHealthQualifiertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
